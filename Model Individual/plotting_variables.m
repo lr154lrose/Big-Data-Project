c72cec877723b7f54884766c5dc7e4ebe41d5137
Ioana Mara <ioana_mara_c@yahoo.com>
2020-03-05 22:36:43 +0000

    added more details to the Europe map such as cities, rivers and lakes

--- Model Individual/plotting_variables.m
+++ Model Individual/plotting_variables.m
@@ -1,6 +1,4 @@
-for i in ['
-
-ncfile = 'W_fr-meteofrance,MODEL,SILAM+FORECAST+SURFACE+O3+0H24H_C_LFPW_20180701000000.nc';
+ncfile = 'first.nc';
 lon = ncread(ncfile,'longitude'); 
 nx = length(lon); 
 lat = ncread(ncfile,'latitude'); 
@@ -18,11 +16,26 @@ Y = double(Y);
 
 for i = 1:length(time)
     unknown = ncread(ncfile,'unknown',[1 1 i],[nx ny 1]);
+    
     load coastlines
     plotm(coastlat,coastlon)
+    
+    land = shaperead('landareas', 'UseGeoCoords', true);
+    geoshow(gca, land, 'FaceColor', [0.5 0.7 0.5])
+
+    lakes = shaperead('worldlakes', 'UseGeoCoords', true);
+    geoshow(lakes, 'FaceColor', 'blue')
+
+    rivers = shaperead('worldrivers', 'UseGeoCoords', true);
+    geoshow(rivers, 'Color', 'blue')
+
+    cities = shaperead('worldcities', 'UseGeoCoords', true);
+    geoshow(cities, 'Marker', '.', 'Color', 'red')
+    
     size(unknown)
     size(X)
     %pcolor(lon,lat,unknown'); 
+    
     surfm(Y, X, unknown', 'EdgeColor', 'none','FaceAlpha', 0.5)
     shading interp
     title("Plotting the nc file variables")
