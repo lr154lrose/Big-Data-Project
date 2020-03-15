ca6c9dbf7882de654f9ceaac8339514f8083c011
lr154lrose <ioana_mara_c@yahoo.com>
2020-03-15 22:15:45 +0000

    added a list selection dialog box so that the user can choose the colour mode of the map

--- Model Individual/plotting_variables.m
+++ Model Individual/plotting_variables.m
@@ -1,4 +1,4 @@
-switch_menu = true;
+answer = 0;
 
 for i = 1:7
     ncfile = strcat(int2str(i),".nc")
@@ -21,16 +21,31 @@ for i = 1:7
     for t = 1:length(time)
         unknown = ncread(ncfile,'unknown',[1 1 t],[nx ny 1]);
         
-        if (switch_menu)
-            answer = menu("color blind mode on or off?",...
-                'on',...
-                'off');
-            if answer==1
+        if (answer == 0)
+            answer = listdlg('PromptString', {'Please select a colour mode.', ...
+                'You can only select one.'}, ...
+                'SelectionMode', 'Single', 'ListString', {'Colour blind', ...
+                'Default', 'Winter', 'Spring', 'Autumn', 'Black & White' ...
+                'Pink', 'Hot', 'Cold'});
+            if answer == 1
                 colormap summer;
             elseif answer == 2
                 colormap default;
+            elseif answer == 3
+                colormap winter;
+            elseif answer == 4
+                colormap spring;
+            elseif answer == 5
+                colormap autumn;
+            elseif answer == 6
+                colormap gray;
+            elseif answer == 7
+                colormap pink;
+            elseif answer == 8 
+                colormap hot;
+            elseif answer == 9
+                colormap cold;
             end
-            switch_menu = false;
         end 
 
         load coastlines
